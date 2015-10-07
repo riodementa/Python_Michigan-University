c0895af52a74543f985ddede3e71c9c26613a104
riodementa <jorge.nayamacias@gmail.com>
2015-10-07 11:51:57 +0200

    Create 5_2.py

---
+++ 5_2.py
@@ -0,0 +1,29 @@
+# Exercise 5.1
+import numpy as np
+
+max=0
+min=0
+list_numbers=[]
+
+while True:
+    str_number = raw_input("Introduce un numero: ")    
+    
+    if str_number=="fin":
+        break
+    
+    try:
+        number = float(str_number)
+        if number>=max:
+            max=number
+        elif number<=min:
+                min=number   
+        else:
+            continue
+                
+    except:
+        print "Entrada inválida" 
+        continue
+    
+    list_numbers.append(number)
+
+print max , min
