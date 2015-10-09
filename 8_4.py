8658def0e5053582b89fe654e2ab712739e487bc
riodementa <jorge.nayamacias@gmail.com>
2015-10-09 12:44:03 +0200

    Update 8_4.py

--- 8_4.py
+++ 8_4.py
@@ -1,1 +1,10 @@
 
+fhand = open("romeo.txt")
+my_list=[]
+for linea in fhand:
+    palabras = linea.strip().split()
+    for palabra in palabras:
+        if not palabra in my_list:
+            my_list.append(palabra)
+my_list.sort()
+print my_list   
