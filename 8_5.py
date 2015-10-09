c3ad61f05da95988e07ba39c7a8626017333288e
riodementa <jorge.nayamacias@gmail.com>
2015-10-09 12:52:23 +0200

    Update 8_5.py

--- 8_5.py
+++ 8_5.py
@@ -1,7 +1,14 @@
-fhand = open("mbox-short.txt")
+filename = raw_input("Introduce un nombre de fichero: (o pulsa ENTER):")
+
+try:
+    fhand = open(filename)
+except:
+    print "Filename not found! Test with mbox-short.txt\n"
+    fhand = open("mbox-short.txt")
+
 counter=0
 for element in fhand:
     if element.startswith("From"):
         counter += 1
         print element.strip().split()[1]
-print "Hay",counter,"lineas en el archivo con From como primera palabra"
+print "\nHay",counter,"lineas en el archivo con From como primera palabra"
